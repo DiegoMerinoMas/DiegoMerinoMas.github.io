86c4a43cced19c9a672b0c869e424808b0ae4ad0
DiegoMerinoMas <u202111473@upc.edu.pe>
2022-12-03 12:40:54 -0500

    actu partido 2

--- main.js
+++
@@ -1,7 +0,0 @@
-const soccerBall = document.querySelector('.soccer-ball');
-
-// Set the cursor position variables on mouse move
-document.addEventListener('mousemove', event => {
-  soccerBall.style.setProperty('--cursor-x', event.clientX + 'px');
-  soccerBall.style.setProperty('--cursor-y', event.clientY + 'px');
-});
